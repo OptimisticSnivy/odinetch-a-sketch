2d12342a40d7134c57a513536081bceb2e754203
Anand Mahamuni <m.anand7203@gmail.com>
2023-12-12 21:23:43 +0530

    Final p2 changes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const colors = ["gold", "royalblue", "red"]
+var colors = ["gold", "royalblue", "red"];
+const buttonColors = ["gold", "royalblue"];
+let temp = 1;
 
 let delegate = document.querySelector(".delegate");
 let gridContainer= document.createElement("div");
@@ -20,7 +22,6 @@ for(let i=0; i<n; i++){
 
 let hover = document.body.querySelector(".delegate");
 hover.addEventListener('mouseover', function(event){
-	console.log("hover");
 	if (event.target.classList.contains("grid")) {
 		let randomColor = colors[Math.floor(Math.random() * colors.length)];
 		event.target.style.backgroundColor = randomColor;
